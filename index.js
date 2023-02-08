39116dfa8d2d272f3a8a4ba26dacde3667183953
mathis-htk <mathis.hantke@gmail.com>
2023-02-08 13:56:44 +0100

    changed

---
+++ index.js
@@ -0,0 +1,10 @@
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Add a click event listener to the button
+btn.addEventListener("click", function() {
+  // Open a popup window
+  alert("Button wurde gedrückt!");
+});
+
+
